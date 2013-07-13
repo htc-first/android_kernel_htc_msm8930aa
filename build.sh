3f3a0865a16f3d71215ee303a07313df4d5abcb5
James Taylor <jmztaylor@gmail.com>
2013-07-13 17:05:08 -0400

    Add correct build script

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-rm output/files/system/lib/modules/*
+rm modules/*.ko
 make clean
 make mrproper
 make operaul_defconfig
 make -j9
-mkdir output
-mkdir output/files/system
-mkdir output/files/system/lib
-mkdir output/files/system/lib/modules
-find . -name '*.ko' -exec cp '{}' output/files/system/lib/modules \;
+find . -name '*.ko' -exec cp {} modules/  \;
