52f7c46e9dbf1e342fed80d28f68b1f94f0ff80e
Bryan Smith <zarboz@gmail.com>
2013-07-23 19:31:58 -0600

    build.sh ADD: use local toolchain instead of one declared in PATH
    Signed-off-by: Bryan Smith <zarboz@gmail.com>

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ rm output/kernel/zImage
 make clean
 make mrproper
 make operaul_defconfig
-make -j9
+make CROSS_COMPILE=android-toolchain-eabi/bin/arm-eab- -j9
 cp arch/arm/boot/zImage output/kernel/zImage
 find . -name '*.ko' -exec cp {} output/system/lib/modules/  \;
 cd output/system/app/
